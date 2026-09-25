6b2b074c51bc158d2b6e2e79a481fbd43383b1f7
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-25 13:40:26 +0300

    changed the entire code

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 #I did everything I could but it would not work so I changed the entire code...
 
-print("Program starting.")
-print()
+print("Program starting.\n")
 
-hexnum = input("Insert a hex color: ")
+hexnum = input("Insert a hex color: \n")
 hexnum = hexnum.lstrip("#")
 
 red = hexnum[0:2]
 green = hexnum[2:4]
 blue = hexnum[4:6]
 
-print("Colors")
-print(f"- Red {red}")
-print(f"- Green {green}")
-print(f"- Blue {blue}")
-print()
-
+print(f"Colors\n- Red {red}\n- Green {green}\n- Blue {blue}\n")
 print("Program ending.")
