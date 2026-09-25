67c7134ac07bada7e983868c87caef91eeee9a4b
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-25 13:32:53 +0300

    changed code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 print("Program starting.\n")
 
-hexnum = input("Insert a hex color: \n")
+hexnum = input("Insert a hex color:\n")
 hexnum = hexnum.lstrip("#")
 
 red = hexnum[0:2]
 green = hexnum[2:4]
 blue = hexnum[4:6]
 
-print("Colors\n- Red", red, "\n- Green", green, "\n- Blue", blue, "\n\nProgram ending.")
+print(f"Colors\n- Red {red}\n- Green {green}\n- Blue {blue}\n")
+print("Program ending.\n")
