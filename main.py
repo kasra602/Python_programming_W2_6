9a9fa0691b9c52e71b70cd3cd90305b4dd808646
Kasra zahedi <106477508+kasra602@users.noreply.github.com>
2026-09-25 13:36:38 +0300

    changed the entire code

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-print("Program starting.\n")
+import sys
 
-hexnum = input("Insert a hex color: \n")
-hexnum = hexnum.lstrip("#")
+sys.stdout.write("Program starting.\n\n")
+
+sys.stdout.write("Insert a hex color: \n")
+hexnum = input().lstrip("#")
 
 red = hexnum[0:2]
 green = hexnum[2:4]
 blue = hexnum[4:6]
 
-print(f"Colors\n- Red {red}\n- Green {green}\n- Blue {blue}\n")
-print("Program ending.\n")
+sys.stdout.write(
+    f"Colors\n"
+    f"- Red {red}\n"
+    f"- Green {green}\n"
+    f"- Blue {blue}\n\n"
+)
+
+sys.stdout.write("Program ending.\n")
